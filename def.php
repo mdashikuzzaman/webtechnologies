0bb0d9fdffc0a0bd07e34f38a84a0c91c06ffbcd
mdashikuzzaman <56515555+mdashikuzzaman@users.noreply.github.com>
2019-10-15 00:20:26 +0600

    Add files via upload

---
+++ def.php
@@ -0,0 +1,14 @@
+<?php
+
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$date = $_POST['date'];
+	$dept = $_POST['dept'];
+	$gender = $_POST['gender'];
+	$option = $_POST['option'];
+	
+	
+	echo $name." ". $password. $date. $dept. $gender. $option;
+	
+
+?>
